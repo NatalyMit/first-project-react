d0c47eab6d725bb43cc73e8c640077549d55e31b
NatalyMit <mitenkova91@icloud.com>
2024-02-26 09:46:26 +0100

    add change

---
+++ src/components/App.jsx
@@ -0,0 +1,59 @@
+// src/App.jsx
+import userData from '../userData.json';
+import Profile from './Profile';
+// import Product from './Product';
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Best selling</h1>
+
+//       <Product
+//         name="Tacos With Lime"
+//         imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+//         price={10.99}
+//       />
+//       <Product
+//         name="Fries and Burger"
+//         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+//         price={14.29}
+//       />
+//     </div>
+//   );
+// }
+// const favouriteBooks = [
+//   { id: 'id-1', name: 'JS for beginners' },
+//   { id: 'id-2', name: 'React basics' },
+//   { id: 'id-3', name: 'React Router overview' },
+// ];
+
+// const BookList = ({ books }) => {
+//   return (
+//     <ul>
+//       {books.map(book => {
+//         return <li key={book.id}>{book.name}</li>;
+//       })}
+//     </ul>
+//   );
+// };
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App;
+
+// <>
+//   <h1>Books of the week</h1>
+//   <BookList books={favouriteBooks} />
+// </>
